7d83b186c22eaf8da2a23f8aecae93046fa9b24d
miky-alt <ertonnozz@gmail.com>
2026-09-04 18:31:37 +0200

    test(experiments): update Adult matrix count

--- tests/scripts/test_adult_preprocessing.py
+++ tests/scripts/test_adult_preprocessing.py
@@ -1,11 +1,12 @@
 import importlib
 
 
-def test_adult_preprocessing_matrix_has_seven_combinations():
+def test_adult_preprocessing_matrix_has_eight_combinations():
     module = importlib.import_module("scripts.run_adult_preprocessing")
 
-    assert len(module.EXPERIMENTS) == 7
+    assert len(module.EXPERIMENTS) == 8
     assert [item["generator_name"] for item in module.EXPERIMENTS] == [
+        "gaussian_copula",
         "gaussian_copula",
         "gaussian_copula",
         "gaussian_copula",
@@ -18,4 +19,8 @@ def test_adult_preprocessing_matrix_has_seven_combinations():
     assert module.EXPERIMENTS[2]["transformer_specs"] == {
         "fnlwgt": {"name": "LogScaler", "kwargs": {}}
     }
+    assert module.EXPERIMENTS[3]["transformer_specs"] == {
+        "fnlwgt": {"name": "LogScaler", "kwargs": {}}
+    }
+    assert module.EXPERIMENTS[3]["generator_kwargs"] == module.EXPERIMENTS[1]["generator_kwargs"]
     assert module.EXPERIMENTS[4]["transformer_specs"] == module.EXPERIMENTS[6]["transformer_specs"]
